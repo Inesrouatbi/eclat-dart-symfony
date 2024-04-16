d6859b0c2a9ff9d43ce84ea0cb4e4b06aa657993
farouk-root <farouk.chalghoumi031@gmail.com>
2024-04-16 15:16:49 +0200

    user register + email Confirmation + userAuth

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -20,7 +22,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column (name : "roles", type : "json")]
     private array $roles = [];
-
+    //composer require symfonycasts/verify-email-bundle
     /**
      * @var string The hashed password
      */
@@ -38,6 +40,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     #[ORM\Column (name : "numTel")]
     private ?int $numTel = null;
+
+    #[ORM\Column(type: 'boolean')]
+    private $isVerified = false;
 
     public function getId(): ?int
     {
@@ -172,6 +177,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setNumTel(int $numTel): static
     {
         $this->numTel = $numTel;
+
+        return $this;
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): static
+    {
+        $this->isVerified = $isVerified;
 
         return $this;
     }
